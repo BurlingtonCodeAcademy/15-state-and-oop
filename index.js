6daf62522c4ed981ef8f29bb58ac60cb3d3c7ce9
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-03-04 18:01:40 -0600

    red light green light state example

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log("state and oop yall!")
+function getPoemTitle(authorUserSelected) {
+    let poemTitlesByAuthor = {
+        "Robert Frost": "The Wild Horse",
+        "Shel Silverstein": "Where the Sidewalk Ends",
+        "Jane Austen": "Pride and Prejudice",
+        "Sylvia Plath": "The Adventures of Sherlock Holmes",
+        "Doctor Seuss": "The Cat In The Hat"
+    }
+    return poemTitlesByAuthor[authorUserSelected];
+}
+
+// console.log(getPoemTitle("Some poet"));
+
+let states = {
+    green: ["yellow"],
+    yellow: ["red"],
+    red: ["green", "yield"],
+    yield: ["red"]
+}
+
+let currentState = "green";
+console.log("You are now in the " + currentState + " state");
+
+function enterState(newState) {
+    let validTransitions = states[currentState];
+    if (validTransitions.includes(newState)) {
+        currentState = newState;
+        console.log("You are now in the " + currentState + " state");
+    } else {
+        throw ("You cannot enter the " + newState + " state from the " + currentState + " state");
+    }
+}
+
+enterState("yellow");
+enterState("red");
+enterState("yield");
+enterState("red");
+enterState("green");
